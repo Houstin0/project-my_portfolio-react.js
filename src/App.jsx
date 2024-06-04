a88adb77528666f48df211dd8ceafa71b7c78757
Houstin0 <houstin0000@gmail.com>
2024-06-04 16:29:11 +0300

    removed earth canvas

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ const App = () => {
         <Works />
         <Feedbacks />
         <div className='relative z-0'>
+       
           <Contact />
          
         </div>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,10 +3,8 @@ import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
 
 import { styles } from "../styles";
-import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
-
 
 
 const Contact = () => {
@@ -70,7 +68,7 @@ const Contact = () => {
 
   return (
     <div
-      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-4 overflow-hidden h-[100vh]`}
+      className={`xl:mt-4 flex xl:flex-row flex-col-reverse gap-4 overflow-hidden h-screen justify-center `}
     >
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
@@ -82,16 +80,16 @@ const Contact = () => {
         <form
   ref={formRef}
   onSubmit={handleSubmit}
-  className='mt-18 flex flex-col gap-4' // Decreased the gap between inputs
+  className='mt-18 flex flex-col gap-4  '
 >
   <label className='flex flex-col'>
-    <span className='text-white font-medium mb-2'>Your Name</span> {/* Decreased the margin bottom */}
+    <span className='text-white font-medium mb-2'>Your Name</span> 
     <input
       type='text'
       name='name'
       value={form.name}
       onChange={handleChange}
-      placeholder="What's your good name?"
+      placeholder="What's your name?"
       className='bg-tertiary py-2 px-4 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium' // Decreased the padding
     />
   </label>
@@ -102,7 +100,7 @@ const Contact = () => {
       name='email'
       value={form.email}
       onChange={handleChange}
-      placeholder="What's your web address?"
+      placeholder="What's your Email address?"
       className='bg-tertiary py-2 px-4 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium' // Decreased the padding
     />
   </label>
@@ -113,7 +111,7 @@ const Contact = () => {
       name='message'
       value={form.message}
       onChange={handleChange}
-      placeholder='What you want to say?'
+      placeholder='What is your Message for me?'
       className='bg-tertiary py-2 px-4 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium' // Decreased the padding
     />
   </label>
@@ -128,12 +126,7 @@ const Contact = () => {
 
       </motion.div>
 
-      <motion.div
-        variants={slideIn("right", "tween", 0.2, 1)}
-        className='xl:flex-1 xl:h-auto md:h-[250px] h-[200px]'
-      >
-        <EarthCanvas />
-      </motion.div>
+     
     </div>
   );
 };

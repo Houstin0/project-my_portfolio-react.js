631fc7d21d53bb6d4f14a9b8c38ceac26c19f106
Houstin0 <houstin0000@gmail.com>
2024-06-04 23:49:10 +0300

    removed ball canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works } from "./components";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Works } from "./components";
 
 const App = () => {
   return (
@@ -12,7 +12,7 @@ const App = () => {
         </div>
         <About />
         <Experience />
-        <Tech />
+        {/* <Tech /> */}
         <Works />
         <Feedbacks />
         <div className='relative z-0'>
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,38 +1,38 @@
 
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 
-import { BallCanvas } from "./canvas";
-import { staggerContainer } from "../utils/motion";
-import { slideIn } from "../utils/motion";
-import { technologies } from "../constants";
+// import { BallCanvas } from "./canvas";
+// import { staggerContainer } from "../utils/motion";
+// import { slideIn } from "../utils/motion";
+// import { technologies } from "../constants";
 
-const Tech = () => {
-  return (
-    <motion.section
-    variants={staggerContainer()}
-    initial='hidden'
-    whileInView='show'
-    viewport={{ once: true, amount: 0.25 }}
-    className="sm:px-8 px-3 sm:py-16 py-10 max-w-7xl mx-auto relative z-0"
-  >
-    <span className='hash-span' id="">
-      &nbsp;
-    </span>
+// const Tech = () => {
+//   return (
+//     <motion.section
+//     variants={staggerContainer()}
+//     initial='hidden'
+//     whileInView='show'
+//     viewport={{ once: true, amount: 0.25 }}
+//     className="sm:px-8 px-3 sm:py-16 py-10 max-w-7xl mx-auto relative z-0"
+//   >
+//     <span className='hash-span' id="">
+//       &nbsp;
+//     </span>
 
-<motion.div variants={slideIn("right", "tween", 0.2, 1)}>
-<div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
-    </div>
-</motion.div>
+// <motion.div variants={slideIn("right", "tween", 0.2, 1)}>
+// <div className='flex flex-row flex-wrap justify-center gap-10'>
+//       {technologies.map((technology) => (
+//         <div className='w-28 h-28' key={technology.name}>
+//           <BallCanvas icon={technology.icon} />
+//         </div>
+//       ))}
+//     </div>
+// </motion.div>
 
-  </motion.section>
+//   </motion.section>
 
-  );
-};
+//   );
+// };
 
-export default Tech
+// export default Tech
